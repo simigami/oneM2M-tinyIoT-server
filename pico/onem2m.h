569de4b88c8d666ee2502ee6caaf32be979cf545
eomkyeongho <rudgh9242@naver.com>
2022-06-28 13:20:20 +0900

    edit header

--- pico/onem2m.h
+++ pico/onem2m.h
@@ -82,7 +82,6 @@ CIN* Retrieve_CIN();
 CSE* Update_CSE(char *json_payload);
 AE* Update_AE(char *json_payload);
 CNT* Update_CNT(char *json_payload);
-CIN* Update_CIN(char *json_payload);
 
 CSE* Delete_CSE();
 AE* Delete_AE();
@@ -99,10 +98,10 @@ char* AE_to_json(AE* ae_object);
 char* CNT_to_json(CNT* cnt_object);
 char* CIN_to_json(CIN* cin_object);
 
-int Load_CSE(CSE* cse_object);
-int Load_AE(AE* ae_object);
-int Load_CNT(CNT* cnt_object);
-int Load_CIN(CIN* cin_object);
+int Store_CSE(CSE* cse_object);
+int Store_AE(AE* ae_object);
+int Store_CNT(CNT* cnt_object);
+int Store_CIN(CIN* cin_object);
 
 CSE* Get_CSE();
 AE* Get_AE();
