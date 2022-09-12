e96ca422b367dba55b4212eb7fb8c4f0aac1a10c
eomkyeongho <rudgh9242@naver.com>
2022-09-13 02:33:14 +0900

    zero-conf API

--- pico/httpd.h
+++ pico/httpd.h
@@ -31,15 +31,15 @@ void bindfd(int slot);
 
 // Response
 #define RESPONSE_PROTOCOL "HTTP/1.1"
-#define RESPONSE_CORS_HEADERS "Access-Control-Allow-Origin: *\nAccess-Control-Allow-Headers: Accept, Accept-Language, Content-Language, Content-Type"
+#define RESPONSE_CORS_HEADERS "Access-Control-Allow-Origin: *\nAccess-Control-Allow-Headers: Accept, Accept-Language, Content-Language, Content-Type\nAccess-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS"
+#define RESPONSE_JSON_HEADERS "Accept: application/json\nContent-Type: application/json; charset=utf-8"
 
-#define HTTP_200_CORS printf("%s 200 OK\n%s\n\n", RESPONSE_PROTOCOL, RESPONSE_HEADERS)
-#define HTTP_201_CORS printf("%s 201 Created\n%s\n\n",RESPONSE_PROTOCOL, RESPONSE_HEADERS)
-#define HTTP_200 printf("%s 200 OK\n\n", RESPONSE_PROTOCOL)
-#define HTTP_201 printf("%s 201 Created\n\n", RESPONSE_PROTOCOL)
-#define HTTP_400 printf("%s 400 Bad Request\n\n", RESPONSE_PROTOCOL)
-#define HTTP_404 printf("%s 404 Not found\n\n", RESPONSE_PROTOCOL)
-#define HTTP_500 printf("%s 500 Internal Server Error\n\n", RESPONSE_PROTOCOL)
+#define HTTP_200_CORS printf("%s 200 OK\n%s\n\n", RESPONSE_PROTOCOL, RESPONSE_CORS_HEADERS)
+#define HTTP_200_JSON printf("%s 200 OK\n%s\n%s\n\n", RESPONSE_PROTOCOL, RESPONSE_CORS_HEADERS, RESPONSE_JSON_HEADERS)
+#define HTTP_201_JSON printf("%s 201 Created\n%s\n%s\n\n", RESPONSE_PROTOCOL, RESPONSE_CORS_HEADERS, RESPONSE_JSON_HEADERS)
+#define HTTP_400 printf("%s 400 Bad Request\n%s\n\n", RESPONSE_PROTOCOL, RESPONSE_JSON_HEADERS)
+#define HTTP_404 printf("%s 404 Not found\n%s\n\n", RESPONSE_PROTOCOL, RESPONSE_JSON_HEADERS)
+#define HTTP_500 printf("%s 500 Internal Server Error\n%s\n\n", RESPONSE_PROTOCOL, RESPONSE_JSON_HEADERS)
 
 // some interesting macro for `route()`
 #define ROUTE_START() if (0) {
